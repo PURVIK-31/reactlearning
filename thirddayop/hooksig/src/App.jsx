d3c5cee5a138c2c26f8eab54cdb3c8ce456b0c10
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-12 16:08:35 +0530

    counter app op :

--- thirddayop/hooksig/src/App.jsx
+++ thirddayop/hooksig/src/App.jsx
@@ -8,26 +8,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div> press here to increase the counter by one : 
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <button onClick={()=> setCount(0)}>reset</button>
+     </div>
     </>
   )
 }
