9204b1568a7d1f308ee410abd2c640c33df81427
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-15 23:59:53 +0530

    kinda navbar

--- fourthig/PROPSLEARNING/src/App.jsx
+++ fourthig/PROPSLEARNING/src/App.jsx
@@ -2,22 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import navbar from "./components/navbar";
-
 
 function App() {
-  let {name,updtaename} = useState("phoe");
-  updtaename("phoegod");
+  const [count, setCount] = useState(0)
 
-let variablee= "phoe";
-let inputgiven = document.querySelector(input)
   return (
     <>
-     hi there 
-     <div>
-      return <h1> the name of the god is : {updtaename(inputgiven)} </h1>
-      </div> 
-      <navbar />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
