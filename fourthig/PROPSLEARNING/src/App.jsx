23bd0450f8ee0196bd5ab7b89620596a0653dc37
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-15 23:57:43 +0530

    kinda navbar

--- fourthig/PROPSLEARNING/src/App.jsx
+++ fourthig/PROPSLEARNING/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import navbar from "./components/navbar";
+
 
 function App() {
-  
+  let {name,updtaename} = useState("phoe");
+  updtaename("phoegod");
 
+let variablee= "phoe";
+let inputgiven = document.querySelector(input)
   return (
     <>
-      
+     hi there 
+     <div>
+      return <h1> the name of the god is : {updtaename(inputgiven)} </h1>
+      </div> 
+      <navbar />
     </>
   )
 }
