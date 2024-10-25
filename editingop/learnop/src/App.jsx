68439c6cb7f8942489058fa6acbbfa9eae699981
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-25 23:29:06 +0530

    done with state hook

--- editingop/learnop/src/App.jsx
+++ editingop/learnop/src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [val,setval] = useState({name:""})
+  const [val,setval] = useState({name:"", email:""})
   return (
     <div className='m-4 p-7'>
       <form action="" onSubmit={(e)=>{
         e.preventDefault()
         console.log(val)
       }}>
-        <input onChange={(e)=>setval({name :e.target.value})} type="text" placeholder= 'WRITE HERE !' />
+        <input onChange={(e)=>setval({...val,name :e.target.value})} type="text" placeholder= 'WRITE HERE !' />
+        <input onChange={(e)=>setval({...val,email :e.target.value})} type="email" placeholder= 'WRITE  mail HERE !' />
         <input type="submit" />
       </form>
     </div>
