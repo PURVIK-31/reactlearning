d7b1fd93bbbb3daeccc3decdee356af303bb8ff0
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-25 23:12:25 +0530

    working with useState hook~

--- editingop/learnop/src/App.jsx
+++ editingop/learnop/src/App.jsx
@@ -1,25 +1,14 @@
-import { useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-export default function App() {
-const handlesubmit = (event)=>{
-event.preventDefault();
-console.log(name.current.value,age.current.value);
-console.log("this concluded the first way of form handling through useref hook in react !")
-}
-const name = useRef(null)
-const age = useRef(null)
+import React, { useState } from 'react'
 
+function App() {
+  const [val,setval] = useState({name:""})
   return (
-  <>
-  <div className='m-4 p-3'>
-   <form action="" onSubmit={handlesubmit}>
-    <input ref={name} type="text" placeholder="Enter your name" />
-    <input ref={age} type="text" placeholder="Enter your age" />
-    <input type="submit" />
-   </form>
-   </div>
- </>
+    <div className='m-4 p-7'>
+      <form action="">
+        <input onChange={(e)=>{console.log(e.target.value)}} type="text" placeholder= 'LIKH YHA MERE BHAI !' />
+      </form>
+    </div>
   )
 }
+
+export default App
