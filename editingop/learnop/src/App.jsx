1cac11a76d180557d3a009e72a2f0814b5670a73
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-25 23:18:26 +0530

    added the code !

--- editingop/learnop/src/App.jsx
+++ editingop/learnop/src/App.jsx
@@ -4,8 +4,12 @@ function App() {
   const [val,setval] = useState({name:""})
   return (
     <div className='m-4 p-7'>
-      <form action="">
-        <input onChange={(e)=>{console.log(e.target.value)}} type="text" placeholder= 'LIKH YHA MERE BHAI !' />
+      <form action="" onSubmit={(e)=>{
+        e.preventDefault()
+        console.log(val)
+      }}>
+        <input onChange={(e)=>setval({name :e.target.value})} type="text" placeholder= 'LIKH YHA MERE BHAI !' />
+        <input type="submit" />
       </form>
     </div>
   )
