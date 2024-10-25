80e98ab0f32021ece4fb77241644397464552e05
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-26 00:18:00 +0530

    structured working

--- editingop/learnop/src/App.jsx
+++ editingop/learnop/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Cards from './components/cards'
 function App() {
   return (
-    <div>
-      
+    <div className=' w-full h-screen bg-zinc-200 flex items-center justify-center'>
+      <div className='container mx-auto '>
+   <Cards />
+     </div>
     </div>
   )
 }
--- editingop/learnop/src/components/cards.jsx
+++ editingop/learnop/src/components/cards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function cards() {
   return (
     <div>
-      
+      hi from card!
     </div>
   )
 }
