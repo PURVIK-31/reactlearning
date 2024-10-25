e83d8c9d467444b9729f42cd0a11b24c3ae059c0
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-25 15:37:56 +0530

    LETS BEGIN TODAY

--- editingop/learnop/src/App.jsx
+++ editingop/learnop/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 export default function App() {
+  const [count, countupdate] = useState(0) 
+  function update(){
+    countupdate(count+1)
+  }
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="text-3xl font-bold bg-gray-700 flex-shrink">
+yohohohohho the state is  {count}
+<button className='m-3 bg-slate-100' onClick={update}> click guruji </button>
     </h1>
   )
 }
