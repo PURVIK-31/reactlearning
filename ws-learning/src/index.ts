1078ce97a51badea6839f205c94a37b1c08e5f10
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-15 16:18:36 +0530

    logging

--- ws-learning/src/index.ts
+++ ws-learning/src/index.ts
@@ -2,5 +2,6 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on("connection", function (socket) {
-  console.log("New client connected");
+  socket.send("hello");
+  console.log("connected");
 });
