ca3df23ace37112d86d6b14771402e2f0ac1a534
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-15 15:58:09 +0530

    control

--- editingop/learnop/src/App.jsx
+++ editingop/learnop/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Cards from './components/cards'
-import Form from './components/form'
+
 function App() {
   return (
-    <div className=' w-full h-screen bg-zinc-200 flex items-center justify-center'>
-      <div className='container mx-auto '>
-       <Cards />
-        <Form />
-     </div>
+    <div className='bg-red-500'>
+      hi there!
     </div>
   )
 }
