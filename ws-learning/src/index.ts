ae151fda0a3c45acf69dae1e0f62b5f4cfab9970
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-15 21:40:14 +0530

    backend done!

--- ws-learning/src/index.ts
+++ ws-learning/src/index.ts
@@ -1,1 +1,12 @@
 // lets create a ping pong server !!
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function (socket) {
+  socket.on("message", (e) => {
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    }
+  });
+});
