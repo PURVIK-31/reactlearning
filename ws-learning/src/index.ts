aa36866bdd405141d4020dfaefcbacce8113402f
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-15 21:25:27 +0530

    start diy

--- ws-learning/src/index.ts
+++ ws-learning/src/index.ts
@@ -1,10 +1,1 @@
-import { WebSocketServer } from "ws";
-const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("connection", function (socket) {
-  socket.send("hello");
-  console.log("connected");
-  socket.on("message", (e) => {
-    console.log(e);
-  });
-});
+// lets create a ping pong server !!
