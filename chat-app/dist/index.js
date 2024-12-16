88ae807cb7be4e9ab12ca0d72d873921e2086df3
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-16 23:24:32 +0530

    basic socket

---
+++ chat-app/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+wss.on("connection", function (socket) {
+    socket.on("message", function (data) {
+        console.log("received: %s", data.toString());
+        socket.send(" (: hello from server ");
+    });
+});
