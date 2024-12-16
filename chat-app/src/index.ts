1823672595db7e56dfd5b6aedb3b1c8a8c590953
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-16 23:50:46 +0530

    model1

--- chat-app/src/index.ts
+++ chat-app/src/index.ts
@@ -1,8 +1,12 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
+const websocketcounter: WebSocket[] = [];
 wss.on("connection", function (socket) {
+  websocketcounter.push(socket);
   socket.on("message", function (data) {
-    console.log("received: %s", data.toString());
-    socket.send(" (: hello from server ");
+    for (let i = 0; i < websocketcounter.length; i++) {
+      console.log("received: %s", data.toString());
+      socket.send(" (: hello from server ");
+    }
   });
 });
