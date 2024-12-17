53eda21f2a5f4cd80beb3008824bd494fd194f1b
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-17 23:14:51 +0530

    backend done basic

--- chat-app/src/index.ts
+++ chat-app/src/index.ts
@@ -9,8 +9,23 @@ wss.on("connection", function (socket) {
   socket.on("message", function (message) {
     const parsedmessage = JSON.parse(message as unknown as string);
     if (parsedmessage.type === "join") {
+      allsockets.push({
+        socket,
+        room: parsedmessage.payload.roomId,
+      });
     }
     if (parsedmessage.type === "chat") {
+      let currentuserroom = null;
+      for (let i = 0; i <= allsockets.length; i++) {
+        if (allsockets[i].socket == socket) {
+          currentuserroom = allsockets[i].room;
+        }
+      }
+      for (let i = 0; i < allsockets.length; i++) {
+        if (allsockets[i].room == currentuserroom) {
+          allsockets[i].socket.send(parsedmessage.payload.message);
+        }
+      }
     }
   });
 });
