de20b1b2ae60eae542a1a5c45c43532df3288f6e
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-17 22:26:16 +0530

    foreach loop implemented !

--- chat-app/src/index.ts
+++ chat-app/src/index.ts
@@ -1,12 +1,11 @@
 import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-const websocketcounter: WebSocket[] = [];
+let websocketcounter: WebSocket[] = [];
 wss.on("connection", function (socket) {
   websocketcounter.push(socket);
   socket.on("message", function (data) {
-    for (let i = 0; i < websocketcounter.length; i++) {
-      console.log("received: %s", data.toString());
-      socket.send(" (: hello from server ");
-    }
+    websocketcounter.forEach((ws) => {
+      ws.send(data.toString());
+    });
   });
 });
