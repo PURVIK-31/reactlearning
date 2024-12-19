5d1e0ddd031f73eac72ec3be07d3f708d57f5cb7
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-19 12:34:15 +0530

    learnt asnyc maybe

---
+++ sqllearning/src/index.ts
@@ -0,0 +1,6 @@
+import { Client } from "pg";
+
+const client = new Client(
+  "postgresql://neondb_owner:PCBUyo1ON5uQ@ep-odd-term-a80555e3.eastus2.azure.neon.tech/neondb?sslmode=require"
+);
+const crafttable = async () => {};
